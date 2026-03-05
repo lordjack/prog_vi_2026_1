0dd8d30a1084054e28b7df79c3ed94fabc84051f
Jackson Meires <lordjackson@gmail.com>
2026-03-05 14:51:33 -0300

    Revert "add novos exercicios revisao JS"

--- revisao_js/pessoa.js
+++ revisao_js/pessoa.js
@@ -5,17 +5,11 @@ export class Pessoa {
     this.telefone = telefone;
   }
 
-  // Lança exceção apenas se menor de idade
   verificarIdade() {
     if (this.idade < 18) {
       throw this.nome + " - pessoa menor de idade";
+    } else {
+      throw this.nome + " - pessoa de maior";
     }
-    console.log(this.nome + " - pessoa de maior");
-  }
-
-  exibirDados() {
-    console.log("Nome: " + this.nome);
-    console.log("Idade: " + this.idade);
-    console.log("Telefone: " + this.telefone);
   }
 }
